1e4eda1e3ca76f27c7c5168a9e8aebcfcef65b38
KirillKolchanov <kirill.kolchanov.work@gmail.com>
2021-01-29 21:40:15 +0100

    Adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,51 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const { src, dest, watch, series, parallel, task } = require('gulp');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const includeFile = require('gulp-file-include');
+const browsersync = require('browser-sync').create();
 
-gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
-      .pipe(sass())
-      .pipe(gulp.dest('app/css'))
-      .pipe(browserSync.reload({
-        stream: true
-      }))
+const SCSS_PATH = './styles/**/*.scss';
+const IMAGES_PATH = './images/**/*.{gif,jpg,png,svg}';
+
+function htmlTask() {
+  return src('index.html')
+    .pipe(includeFile({ prefix: '@' }))
+    .pipe(dest('dist'));
+}
+
+function scssTask() {
+  return src(SCSS_PATH, { sourcemaps: true })
+    .pipe(sourcemaps.init())
+    .pipe(concat('styles.css'))
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(dest('dist'));
+}
+
+function imagesTask() {
+  return src(IMAGES_PATH)
+    .pipe(dest('dist/images'));
+}
+
+function serveTask(cb) {
+  browsersync.init({
+    server: {
+      baseDir: 'dist'
+    }
   });
+  cb();
+}
 
-  gulp.task('browserSync', function() {
-    browserSync.init({
-      server: {
-        baseDir: 'app'
-      },
-    })
-  })
+function reloadTask(cb) {
+  browsersync.reload();
+  cb();
+}
 
-  gulp.task('watch', gulp.series(['browserSync', 'sass'], function (){
-    gulp.watch('app/scss/**/*.scss', gulp.series('sass'));
-  }))
+function watchTask(){
+  watch('*.html', series(htmlTask, reloadTask));
+  watch(SCSS_PATH, series(scssTask, reloadTask));
+  watch(IMAGES_PATH, series(imagesTask, reloadTask));
+}
 
+task('default', series(parallel(scssTask, imagesTask, htmlTask), serveTask, watchTask));
